abc49fadb277552d28ae379644ee48ab0e9026f5
user.name <gmail@hdknr.com>
2021-03-04 21:50:16 +0900

    sshd方式は課金で優位かも

--- bin/push_ecr.bash
+++ bin/push_ecr.bash
@@ -5,8 +5,9 @@ REGION=ap-northeast-1
 PROFILE=spindd
 AWS_ID=$1               # AWS ID
 ECR=$AWS_ID.dkr.ecr.$REGION.amazonaws.com
+DOCKERFILE=Dockerfile
 #
 aws ecr get-login-password --profile $PROFILE --region $REGION  | docker login --username AWS --password-stdin $ECR
-docker build -t $REPO -f docker/deployment/Dockerfile .
+docker build -t $REPO -f docker/deployment/$DOCKERFILE .
 docker tag $REPO:latest $ECR/$REPO:latest
 docker push $ECR/$REPO:latest
