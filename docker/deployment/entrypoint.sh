3d320c00baae0d96d9eadfee42d8d5009d95309f
user.name <gmail@hdknr.com>
2021-03-04 20:30:00 +0900

    docker run CMD argument replace CMD in Dockerfile on up and running

--- docker/deployment/entrypoint.sh
+++ docker/deployment/entrypoint.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 if [ -L /etc/nginx/sites-enabled/default ]; then
    unlink /etc/nginx/sites-enabled/default
 fi
 service ssh restart
 service nginx start
+if [ "$1" != "" ]; then
+   /usr/local/bin/python /usr/src/app/web/manage.py pages make-message $1
+fi
 /usr/local/bin/gunicorn -c /usr/src/app/web/gunicorn.py app.wsgi:application
 # exec "$@"
