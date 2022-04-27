0a3a321e1461dd2241679cc438e27d85f7fc2352
user.name <gmail@hdknr.com>
2022-04-27 14:06:40 +0900

    gunicorn settings

--- web/gunicorn.py
+++ web/gunicorn.py
@@ -1,27 +1,30 @@
-'''
+"""
 gunicorn -c gunicorn.py  app.wsgi:application
-'''
-import gunicorn
-gunicorn.SERVER_SOFTWARE = 'Gunicorn'
-
-import os
+"""
 import sys
 
+import environ
+import gunicorn
+
+BASE_DIR = environ.Path(__file__) - 1
+PROJECT_DIR = BASE_DIR - 1  #
+ENV = environ.Env()
+ENV.read_env(env_file=str(PROJECT_DIR.path(".env")))
+
+
 # for Gunicorn to find app.wsgi:application
-DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(DIR)
+sys.path.append(str(BASE_DIR))
 
 # logs
 # http://gunicorn-docs.readthedocs.org/en/latest/deploy.html#logging
 #
 #  kill -USR1 $(cat /var/run/gunicorn.pid)
 #
-#LOGS = os.path.join(os.path.dirname(DIR), 'logs')
-#if not os.path.isdir(LOGS):
-#    os.makedirs(LOGS)
 
 # Gunicorn Configuration
-timeout = 180
-bind = "unix:/var/run/gunicorn/,"
-#bind = "unix:" + os.path.join(LOGS, "gunicorn.sock")
-#bind = "0.0.0.0:80"   # USE TCP
+gunicorn.SERVER_SOFTWARE = ENV("GUNICORN_PROCESS", default="Gunicorn")
+timeout = ENV.int("GUNICORN_TIMEOUT", default=180)
+bind = ENV.str("GUNICORN_BIND", default=f"unix:{PROJECT_DIR}/logs/gunicorn.sock")
+pidfile = ENV.str("GUNICORN_PIDFILE", default=f"{PROJECT_DIR}/logs/gunicorn.pid")
+accesslog = ENV.str("GUNICORN_ACCESSLOG", default="-") or "-"
+errorlog = ENV.str("GUNICORN_ERRORLOG", default="-") or "-"
