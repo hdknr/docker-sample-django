7b5ee00145ca606b00cc15ca4f0f9ffcdecdf173
user.name <gmail@hdknr.com>
2022-04-27 14:05:30 +0900

    remove ssh

--- docker/deployment/entrypoint.sh
+++ docker/deployment/entrypoint.sh
@@ -2,6 +2,6 @@
 if [ "$1" != "" ]; then
    /usr/local/bin/python /usr/src/app/web/manage.py pages make-message $1
 fi
-
+/etc/init.d/nginx restart
 /usr/local/bin/gunicorn -c /usr/src/app/web/gunicorn.py app.wsgi:application &
 sleep infinity 
